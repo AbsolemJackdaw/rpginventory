76faba32ac132dff68eab50bb8630796703c2bee
rich1051414@gmail.com <rich1051414@gmail.com>
2013-03-09 16:43:02 -0600

    Fixed inventory and started on new gui implimentation

--- common/RpgInventory/ClientTickHandler.java
+++ common/RpgInventory/ClientTickHandler.java
@@ -1,14 +1,25 @@
 package RpgInventory;
 
+import RpgInventory.gui.ButtonInventory;
 import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.TickType;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.gui.inventory.GuiContainerCreative;
 import net.minecraft.client.renderer.InventoryEffectRenderer;
+import net.minecraft.potion.Potion;
 
 public class ClientTickHandler implements ITickHandler {
+
+    boolean added = false;
 
     @Override
     public void tickStart(EnumSet<TickType> type, Object... tickData) {
@@ -16,13 +27,44 @@ public class ClientTickHandler implements ITickHandler {
 
     @Override
     public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+        //This will only inject our buttons into the existing GuiInventory object.
+        //The button prevents calls to the parent GUI if clicked, and calls our packet
+        //instead. I see no incompatibilies.
         if (Minecraft.getMinecraft().currentScreen != null && Minecraft.getMinecraft().theWorld != null && Minecraft.getMinecraft().thePlayer != null) {
             if (Minecraft.getMinecraft().currentScreen instanceof GuiInventory) {
-                //TODO:Add Button
-                //GuiInventory gui = (GuiInventory)Minecraft.getMinecraft().currentScreen;
-                //gui.controlList.add(gui);
+                if (!added) {
+                    added = true;
+                    GuiInventory gui = (GuiInventory) Minecraft.getMinecraft().currentScreen;
+                    List currentButtons = null;
+                    for (Field f : GuiScreen.class.getDeclaredFields()) {
+                        f.setAccessible(true);
+                        try {
+                            //"controlList" is the name deobfuscated. "i" is the name obfustcated.
+                            //This adds compatibility when developing AND on release.
+                            if (f.getName().equals("controlList") || f.getName().equals("i")) {
+                                Field modfield = Field.class.getDeclaredField("modifiers");
+                                modfield.setAccessible(true);
+                                modfield.setInt(f, f.getModifiers() & ~Modifier.PROTECTED);
+                                currentButtons = (List) f.get(gui);
+                                int offsetx = 85;
+                                int offsety = 90;
+                                int posX = (Minecraft.getMinecraft().currentScreen.width) / 2;
+                                int posY = (Minecraft.getMinecraft().currentScreen.height) / 2;
+                                currentButtons.add(new ButtonInventory(currentButtons.size() + 1, posX - offsetx, posY + offsety, "Rpg Inventory"));
+                                currentButtons.add(new ButtonInventory(currentButtons.size() + 1, posX - offsetx + 80 + 5, posY + offsety, "Close"));
+                            }
+                        } catch (Exception e) {
+                            System.err.println("Severe error, please report this to the mod author:");
+                            System.err.println(e);
+                        }
+                    }
+                }
             }
+
+        }else{
+            added = false;
         }
+        //Area below reserved for future client tick stuff.
     }
 
     @Override
