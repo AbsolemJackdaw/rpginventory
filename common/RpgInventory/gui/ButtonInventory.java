25a9f7cd4dc15892ec5b2eba930306788b5a91e8
rich1051414@gmail.com <rich1051414@gmail.com>
2013-03-09 16:43:12 -0600

    Fixed inventory and started on new gui implimentation

--- common/RpgInventory/gui/ButtonInventory.java
+++ common/RpgInventory/gui/ButtonInventory.java
@@ -21,33 +21,52 @@ import net.minecraft.network.packet.Packet250CustomPayload;
  */
 @SideOnly(Side.CLIENT)
 public class ButtonInventory extends GuiButton {
+
     private EntityPlayer player;
     public ButtonInventory(int par1, int par2, int par3, String par4Str) {
         super(par1, par2, par3, par4Str);
         player = Minecraft.getMinecraft().thePlayer;
+        this.width = 80;
+        this.height = 20;
+        this.drawButton = true;
+        this.enabled = true;
     }
 
     @Override
-    public void mouseReleased(int par1, int par2) {
-        super.mouseReleased(par1, par2);
-        switch(this.id){
-            case 0:
-                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            DataOutputStream outputStream = new DataOutputStream(bytes);
-            try {
-                outputStream.writeInt(1);
-                Packet250CustomPayload packet = new Packet250CustomPayload("RpgInv", bytes.toByteArray());
-                PacketDispatcher.sendPacketToServer(packet);
-                System.out.println("Packet send");
-            } catch (IOException e) {
-                e.printStackTrace();
+    public boolean mousePressed(Minecraft par1Minecraft, int par2, int par3) {
+        
+        if (par2 >= this.xPosition && par2 <= this.xPosition + this.width) {
+            if (par3 >= this.yPosition && par3 <= this.yPosition + this.height) {
+                
+                if (this.displayString.equals("Rpg Inventory")) {
+                    
+                    System.out.println("called");
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    DataOutputStream outputStream = new DataOutputStream(bytes);
+                    try {
+                        outputStream.writeInt(1);
+                        Packet250CustomPayload packet = new Packet250CustomPayload("RpgInv", bytes.toByteArray());
+                        PacketDispatcher.sendPacketToServer(packet);
+                        System.out.println("Packet send");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if (this.displayString.equals("Close")) {
+                    player.closeScreen();
+                }
+                Minecraft.getMinecraft().sndManager.playSoundFX("random.click", 1.0F, 1.0F);
+                return false;
             }
-                break;
-            case 1:
-                player.closeScreen();
-                break;
-            case 2:
-                //TODO:Add pet shortcut
         }
+        return super.mousePressed(par1Minecraft, par2, par3);
     }
+
+    @Override
+    public void func_82251_b(int par1, int par2) {
+        System.out.println("woot");
+        super.func_82251_b(par1, par2);
+    }
+
+    
+    
 }
