79d0f6cd8b414df88079ce03bda0b6aeadd3b799
Axel Cornelis <robbeenaxel@hotmail.com>
2013-03-10 17:24:00 +0100

    petgui changed. commiting to be able to pull

--- common/RpgInventory/gui/pet/PetGui.java
+++ common/RpgInventory/gui/pet/PetGui.java
@@ -14,7 +14,9 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.packet.Packet250CustomPayload;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 import org.lwjgl.opengl.GL11;
@@ -29,98 +31,128 @@ import net.minecraft.item.ItemStack;
 
 public class PetGui extends GuiScreen {
 
-    BMPetImpl thePet;
-    int petType; /*Spider 3, bull 2, boar 1*/
+	BMPetImpl thePet;
+	int petType; /*Spider 3, bull 2, boar 1*/
 
-    EntityPlayer p;
-    ItemStack petCrystal;
+	EntityPlayer p;
+	ItemStack petCrystal;
 
-    public PetGui(EntityPlayer p1) {
-        petCrystal = mod_RpgInventory.proxy.getInventory(p1.username).getCrystal();
-        if (IPet.playersWithActivePets.containsKey(p1.username)) {
-            thePet = (BMPetImpl) IPet.playersWithActivePets.get(p1.username);
-        }
-        p = p1;
-        if (thePet == null || ((EntityLiving) thePet).isDead) {
-            petType = petCrystal.getItemDamage();
-        }
-    }
-    private GuiTextField textfield;
-    public final int xSizeOfTexture = 181;
-    public final int ySizeOfTexture = 156;
+	public PetGui(EntityPlayer p1) {
+		petCrystal = mod_RpgInventory.proxy.getInventory(p1.username).getCrystal();
+		if (IPet.playersWithActivePets.containsKey(p1.username)) {
+			thePet = (BMPetImpl) IPet.playersWithActivePets.get(p1.username);
+		}
+		p = p1;
+		if (thePet == null || ((EntityLiving) thePet).isDead) {
+			petType = petCrystal.getItemDamage();
+		}
+	}
+	private GuiTextField textfield;
+	public final int xSizeOfTexture = 181;
+	public final int ySizeOfTexture = 166;
+	public static String petLevel = "Level";
+	public static String petName = "Name";
+	public static String petStats = "Stats :";
+	public static String info = "Additional Info";
+	public static String saddle;
 
-    public void initGui() {
+	public void initGui() {
 
-        int var4 = this.mc.renderEngine.getTexture("/subaraki/petgui.png");
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.renderEngine.bindTexture(var4);
+		int var4 = this.mc.renderEngine.getTexture("/subaraki/petgui.png");
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.mc.renderEngine.bindTexture(var4);
 
-        int posX = (this.width - xSizeOfTexture) / 2;
-        int posY = (this.height - ySizeOfTexture) / 2;
-        controlList.clear();
-        controlList.add(new GuiButton(1, posX + 90, posY + 100, 70, 20, "Done"));
-        controlList.add(new GuiButton(2, posX + 235, posY + 1, 20, 20, "X"));
+		int posX = (this.width - xSizeOfTexture) / 2;
+		int posY = (this.height - ySizeOfTexture) / 2;
+		controlList.clear();
+		controlList.add(new GuiButton(1, posX+ 70 , posY+36  , 40, 20, "Submit"));
+		controlList.add(new GuiButton(3, posX + 190, posY + 22, 30, 20, "Back"));
+		controlList.add(new GuiButton(2, posX+ 190 , posY     , 30, 20, "Close"));
 
-        textfield = new GuiTextField(fontRenderer, posX + 68, posY + 60, 120, 20);
-        textfield.setText(petCrystal.getTagCompound().getString("PetName"));
-        textfield.setMaxStringLength(32);
+		textfield = new GuiTextField(fontRenderer, posX + 70, posY + 14, 100, 20);
+		textfield.setText(petCrystal.getTagCompound().getString("PetName"));
+		textfield.setMaxStringLength(32);
 
-    }
+	}
 
-    protected void actionPerformed(GuiButton guibutton) {
-        if (guibutton.id == 1) {
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            ObjectOutput out;
-            try {
-                out = new ObjectOutputStream(bytes);
-                System.out.println(textfield.getText());
-                out.writeUTF(textfield.getText());
-                out.close();
-                Packet250CustomPayload packet = new Packet250CustomPayload("RpgRBPacket", bytes.toByteArray());
-                PacketDispatcher.sendPacketToServer(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("nameSendFailed");
+	protected void actionPerformed(GuiButton guibutton) {
+		if (guibutton.id == 1) {
+			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+			ObjectOutput out;
+			try {
+				out = new ObjectOutputStream(bytes);
+				System.out.println(textfield.getText());
+				out.writeUTF(textfield.getText());
+				out.close();
+				Packet250CustomPayload packet = new Packet250CustomPayload("RpgRBPacket", bytes.toByteArray());
+				PacketDispatcher.sendPacketToServer(packet);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("nameSendFailed");
 
-            }
-            this.mc.thePlayer.closeScreen();
+			}
+			this.mc.thePlayer.closeScreen();
 
-        }
-        if (guibutton.id == 2) {
-            this.mc.thePlayer.closeScreen();
-        }
-    }
+		}
+		if (guibutton.id == 2) {
+			this.mc.thePlayer.closeScreen();
+		}
+	}
 
-    protected void keyTyped(char c, int i) {
-        super.keyTyped(c, i);
-        textfield.textboxKeyTyped(c, i);
-    }
+	protected void keyTyped(char c, int i) {
+		super.keyTyped(c, i);
+		textfield.textboxKeyTyped(c, i);
+	}
 
-    protected void mouseClicked(int i, int j, int k) {
-        super.mouseClicked(i, j, k);
-        textfield.mouseClicked(i, j, k);
-    }
+	protected void mouseClicked(int i, int j, int k) {
+		super.mouseClicked(i, j, k);
+		textfield.mouseClicked(i, j, k);
+	}
 
-    public boolean doesGuiPauseGame() {
-        return false;
-    }
+	public boolean doesGuiPauseGame() {
+		return false;
+	}
 
-    public void onGuiClosed() {
-        
-    }
+	public void onGuiClosed() {
 
-    public void drawScreen(int i, int j, float f) {
-        drawDefaultBackground();
-        try {
-            int var4 = this.mc.renderEngine.getTexture("/subaraki/petgui.png");
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            this.mc.renderEngine.bindTexture(var4);
-            int posX = (this.width - xSizeOfTexture) / 2;
-            int posY = (this.height - ySizeOfTexture) / 2;
-            drawTexturedModalRect(posX, posY, 0, 0, xSizeOfTexture, ySizeOfTexture);
-        } finally {
-            textfield.drawTextBox();
-        }
-        super.drawScreen(i, j, f);
-    }
-}
+	}
+
+	public void drawScreen(int i, int j, float f) {
+		drawDefaultBackground();
+		NBTTagCompound tags = petCrystal.getTagCompound();
+		
+		if (tags.hasKey("PetLevel")) {
+			if (tags.getInteger("PetLevel") >= 50) {
+				if (tags.hasKey("isSaddled")) {
+					if (tags.getBoolean("isSaddled") == true) {
+						saddle = tags.getString("PetName") + " is saddled.";				
+					}
+					if (tags.getBoolean("isSaddled") == false) {
+						saddle = tags.getString("PetName") + " needs a Saddle to be ridden.";
+					}
+				}
+			} else {
+				saddle = tags.getString("PetName") + " can not be ridden yet. Lvl 50 requiered.";
+			}
+		}
+			
+			try {
+				int var4 = this.mc.renderEngine.getTexture("/subaraki/petgui.png");
+				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+				this.mc.renderEngine.bindTexture(var4);
+				int posX = (this.width - xSizeOfTexture) / 2;
+				int posY = (this.height - ySizeOfTexture) / 2;
+				drawTexturedModalRect(posX, posY, 0, 0, xSizeOfTexture, ySizeOfTexture);
+			} finally {
+				textfield.drawTextBox();
+			}
+			drawString(fontRenderer, petName, this.width / 2 -20, this.height / 2-79 , 0xffffff);
+			drawString(fontRenderer, petStats, this.width / 2-85, this.height / 2-10, 0x00ffff);
+			drawString(fontRenderer, "Lvl : "+ tags.getString(String.valueOf(tags.getInteger("PetLevel"))),this.width / 2, this.height / 2, 0xffffff);
+
+			drawString(fontRenderer, info, this.width / 2-85, this.height / 2+45, 0xff00ff);
+			drawString(fontRenderer,saddle, this.width / 2-80, this.height / 2+55, 0xffffff);
+
+			super.drawScreen(i, j, f);
+		}
+	}
