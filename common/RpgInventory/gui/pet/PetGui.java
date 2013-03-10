ed8706978d2dbe5c2a90823401b89acde6c7c19e
Axel Cornelis <robbeenaxel@hotmail.com>
2013-03-10 22:44:24 +0100

    Pet gui imbue option

--- common/RpgInventory/gui/pet/PetGui.java
+++ common/RpgInventory/gui/pet/PetGui.java
@@ -1,34 +1,28 @@
 package RpgInventory.gui.pet;
 
-import RpgInventory.IPet;
-import RpgInventory.mod_RpgInventory;
-import RpgRB.beastmaster.BMPetImpl;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.packet.Packet250CustomPayload;
-import net.minecraft.util.StatCollector;
-import net.minecraft.world.World;
+import net.minecraft.util.MathHelper;
 
 import org.lwjgl.opengl.GL11;
 
-import RpgRB.beastmaster.BoarPet;
-import RpgRB.beastmaster.BullPet;
-import RpgRB.beastmaster.SpiderPet;
+import RpgInventory.IPet;
+import RpgInventory.mod_RpgInventory;
+import RpgRB.beastmaster.BMPetImpl;
 import cpw.mods.fml.common.network.PacketDispatcher;
-import cpw.mods.fml.common.network.Player;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.item.ItemStack;
 
 public class PetGui extends GuiScreen {
 
@@ -60,6 +54,8 @@ public class PetGui extends GuiScreen {
 	public static String currentHP ;
 	public static String totalHP ;
 	public static String saddle;
+	public static String levelInfo;
+	public static String levelInfo2;
 
 	public void initGui() {
 
@@ -71,13 +67,15 @@ public class PetGui extends GuiScreen {
 		int posY = (this.height - ySizeOfTexture) / 2;
 		controlList.clear();
 		controlList.add(new GuiButton(1, posX+ 70 , posY+36  , 40, 20, "Submit"));
-		controlList.add(new GuiButton(3, posX + 190, posY + 22, 30, 20, "Back"));
-		controlList.add(new GuiButton(2, posX+ 190 , posY     , 30, 20, "Close"));
+		controlList.add(new GuiButton(4, posX+ 182 , posY+120 , 40, 20, "Imbue"));
+		controlList.add(new GuiButton(3, posX + 182, posY + 22, 30, 20, "Back"));
+		controlList.add(new GuiButton(2, posX+ 182 , posY     , 30, 20, "Close"));
 
 		textfield = new GuiTextField(fontRenderer, posX + 70, posY + 14, 100, 20);
 		textfield.setText(petCrystal.getTagCompound().getString("PetName"));
 		textfield.setMaxStringLength(32);
-
+		levelInfo = "";
+		levelInfo2 = "";
 	}
 
 	protected void actionPerformed(GuiButton guibutton) {
@@ -96,27 +94,42 @@ public class PetGui extends GuiScreen {
 				System.out.println("nameSendFailed");
 
 			}
-			this.mc.thePlayer.closeScreen();
-
 		}
 		if (guibutton.id == 2) {
 			this.mc.thePlayer.closeScreen();
 		}
 		if (guibutton.id == 3) {
 			int i = 1;
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            ObjectOutput out;
-            DataOutputStream outputStream = new DataOutputStream(bytes);
-            try {
-                outputStream.writeInt(i);
-                Packet250CustomPayload packet = new Packet250CustomPayload("RpgInv", bytes.toByteArray());
-                PacketDispatcher.sendPacketToServer(packet);
-                //System.out.println("Packet send");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+			ObjectOutput out;
+			DataOutputStream outputStream = new DataOutputStream(bytes);
+			try {
+				outputStream.writeInt(i);
+				Packet250CustomPayload packet = new Packet250CustomPayload("RpgInv", bytes.toByteArray());
+				PacketDispatcher.sendPacketToServer(packet);
+				//System.out.println("Packet send");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+		if (guibutton.id == 4) {
+			levelInfo = "";
+			levelInfo2 = "";
+			if(Minecraft.getMinecraft().thePlayer.experienceLevel < petCrystal.getTagCompound().getInteger("PetLevel")/2)
+			{
+				levelInfo = "You need "+ MathHelper.floor_float(petCrystal.getTagCompound().getInteger("PetLevel")/2) +" levels to" ;
+				levelInfo2 = "level your pet.";
+			}
 
+			else if (Minecraft.getMinecraft().thePlayer.experienceLevel >= petCrystal.getTagCompound().getInteger("PetLevel")/2)
+			{
+				levelInfo = "Added 1 pet Level";
+				levelInfo2 = "Cost: "+ MathHelper.floor_float(petCrystal.getTagCompound().getInteger("PetLevel")/2)+" player levels;" ;
+				Minecraft.getMinecraft().thePlayer.addExperienceLevel(-(MathHelper.floor_float(petCrystal.getTagCompound().getInteger("PetLevel")/2)));
+				petCrystal.getTagCompound().setInteger("PetLevel", petCrystal.getTagCompound().getInteger("PetLevel")+1);
+			}
+
+		}
 	}
 
 	protected void keyTyped(char c, int i) {
@@ -141,8 +154,8 @@ public class PetGui extends GuiScreen {
 		drawDefaultBackground();
 		NBTTagCompound tags = petCrystal.getTagCompound();
 
-		levelNr = petCrystal.getTagCompound().getString(String.valueOf(tags.getInteger("PetLevel")));
-		petAtk  = petCrystal.getTagCompound().getString(String.valueOf(tags.getInteger("PetAttack")));
+		levelNr = String.valueOf(tags.getInteger("PetLevel"));
+		petAtk  = String.valueOf(tags.getInteger("PetAttack"));
 		currentHP = String.valueOf(tags.getInteger("PetPrevHealth"));
 		totalHP   = String.valueOf(tags.getInteger("PetHealth"));
 
@@ -158,7 +171,7 @@ public class PetGui extends GuiScreen {
 					}
 				}
 			} else {
-				saddle = tags.getString("PetName") + " can not be ridden yet. Lvl 50 requiered.";
+				saddle ="Needs level 50 to be ridden." ;
 			}
 		}
 
@@ -180,6 +193,9 @@ public class PetGui extends GuiScreen {
 
 		drawString(fontRenderer, info, this.width / 2-85, this.height / 2+45, 0xff00ff);
 		drawString(fontRenderer,saddle, this.width / 2-80, this.height / 2+55, 0xffffff);
+
+		drawString(fontRenderer,levelInfo, this.width / 2+95, this.height / 2+65, 0xffffff);
+		drawString(fontRenderer,levelInfo2, this.width / 2+95, this.height / 2+75, 0xffffff);
 
 		super.drawScreen(i, j, f);
 	}
