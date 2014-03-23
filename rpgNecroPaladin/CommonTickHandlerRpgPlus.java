34aca0578a7b539283813a7840567b531c713f94
Axel Cornelis <robbeenaxel@hotmail.com>
2014-03-23 14:02:36 +0100

    events

--- rpgNecroPaladin/CommonTickHandlerRpgPlus.java
+++ rpgNecroPaladin/CommonTickHandlerRpgPlus.java
@@ -4,17 +4,20 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-public class CommonTickHandlerRpgPlus implements ITickHandler {
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+
+public class CommonTickHandlerRpgPlus /*implements ITickHandler*/ {
 
 	public static HashMap<String, Integer> rpgPluscooldownMap = new HashMap();
 
-	@Override
-	public String getLabel() {
-		return "RpgInventoryServ";
-	}
+//	@Override
+//	public String getLabel() {
+//		return "RpgInventoryServ";
+//	}
 
-	@Override
-	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+	@SubscribeEvent
+	public void tickEnd(TickEvent.ServerTickEvent ev) {
 		for (Entry<String, Integer> entry : rpgPluscooldownMap.entrySet()) {
 			if (entry.getValue() > 0) {
 				entry.setValue(entry.getValue() - 1);
@@ -22,15 +25,15 @@ public class CommonTickHandlerRpgPlus implements ITickHandler {
 		}
 	}
 
-	/**
-	 * called upon player's death. Will drop Jewels in the world
-	 */
-	@Override
-	public EnumSet<TickType> ticks() {
-		return EnumSet.of(TickType.SERVER);
-	}
-
-	@Override
-	public void tickStart(EnumSet<TickType> type, Object... tickData) {
-	}
+//	/**
+//	 * called upon player's death. Will drop Jewels in the world
+//	 */
+//	@Override
+//	public EnumSet<TickType> ticks() {
+//		return EnumSet.of(TickType.SERVER);
+//	}
+//
+//	@Override
+//	public void tickStart(EnumSet<TickType> type, Object... tickData) {
+//	}
 }
