fbf6c29afeaf56f7c682fc02e90acb2b1b61aed7
ShadowChild <shadychild12@gmail.com>
2014-03-23 20:12:50 +0000

    RpgPlus packets updated
    Signed-off-by: ShadowChild <shadychild12@gmail.com>

--- rpgNecroPaladin/packets/PacketNecroSpecial.java
+++ rpgNecroPaladin/packets/PacketNecroSpecial.java
@@ -1,36 +1,36 @@
-package rpgNecroPaladin.packets;
-
-import java.io.DataInputStream;
-import java.util.List;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import rpgInventory.mod_RpgInventory;
-import rpgInventory.gui.rpginv.PlayerRpgInventory;
-import rpgNecroPaladin.mod_RpgPlus;
-import rpgNecroPaladin.minions.IMinion;
-import rpgNecroPaladin.minions.MinionRegistry;
-
-public class PacketNecroSpecial {
-
-	public PacketNecroSpecial(ItemStack weapon, DataInputStream dis,
-			PlayerRpgInventory inv, EntityPlayer p) {
-		try {
-			dis.close();
-		} catch (Throwable ex) {
-			ex.printStackTrace();
-		}
-
-		if (weapon.getItem().equals(mod_RpgPlus.necro_weapon)
-				&& mod_RpgInventory.playerClass
-						.contains(mod_RpgPlus.CLASSNECRO)) {
-			if (MinionRegistry.playerMinions.containsKey(p.getDisplayName())) {
-				List<IMinion> list = MinionRegistry.playerMinions.get(p
-						.getDisplayName());
-				for (IMinion minion : list) {
-					minion.Harvest();
-				}
-			}
-		}
-	}
-}
+//package rpgNecroPaladin.packets;
+//
+//import java.io.DataInputStream;
+//import java.util.List;
+//
+//import net.minecraft.entity.player.EntityPlayer;
+//import net.minecraft.item.ItemStack;
+//import rpgInventory.mod_RpgInventory;
+//import rpgInventory.gui.rpginv.PlayerRpgInventory;
+//import rpgNecroPaladin.mod_RpgPlus;
+//import rpgNecroPaladin.minions.IMinion;
+//import rpgNecroPaladin.minions.MinionRegistry;
+//
+//public class PacketNecroSpecial {
+//
+//	public PacketNecroSpecial(ItemStack weapon, DataInputStream dis,
+//			PlayerRpgInventory inv, EntityPlayer p) {
+//		try {
+//			dis.close();
+//		} catch (Throwable ex) {
+//			ex.printStackTrace();
+//		}
+//
+//		if (weapon.getItem().equals(mod_RpgPlus.necro_weapon)
+//				&& mod_RpgInventory.playerClass
+//						.contains(mod_RpgPlus.CLASSNECRO)) {
+//			if (MinionRegistry.playerMinions.containsKey(p.getDisplayName())) {
+//				List<IMinion> list = MinionRegistry.playerMinions.get(p
+//						.getDisplayName());
+//				for (IMinion minion : list) {
+//					minion.Harvest();
+//				}
+//			}
+//		}
+//	}
+//}
--- rpgNecroPaladin/packets/RpgPlusPacketHandler.java
+++ rpgNecroPaladin/packets/RpgPlusPacketHandler.java
@@ -1,63 +1,63 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package rpgNecroPaladin.packets;
-
-import ibxm.Player;
-
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import rpgInventory.gui.rpginv.PlayerRpgInventory;
-
-/**
- * 
- * @author Home
- */
-public class RpgPlusPacketHandler implements IPacketHandler {
-
-	public static class WEAPONIDS {
-
-		public static final int SKULLRCLICK = 5;
-		public static final int NECROSPECIAL = 6;
-		public static final int PALADINSPECIAL = 9;
-	}
-
-	@Override
-	public void onPacketData(INetworkManager manager,
-			Packet250CustomPayload packet, Player player) {
-		//
-		DataInputStream dis = new DataInputStream(new ByteArrayInputStream(
-				packet.data));
-		int weaponID = 0;
-		EntityPlayer p = (EntityPlayer) player;
-		try {
-			weaponID = dis.readInt();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-		ItemStack weapon = p.getCurrentEquippedItem();
-		PlayerRpgInventory inv = PlayerRpgInventory.get(p);
-
-		if (inv != null) {
-			switch (weaponID) {
-
-			case WEAPONIDS.SKULLRCLICK:
-				new PacketSpawnMinion(weapon, dis, inv, p);
-				break;
-			case WEAPONIDS.NECROSPECIAL:
-				new PacketNecroSpecial(weapon, dis, inv, p);
-				break;
-			case WEAPONIDS.PALADINSPECIAL:
-				new PacketPalaSpecial(player, p, dis, weapon, inv);
-				break;
-			default:
-				break;
-			}
-		}
-	}
-}
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package rpgNecroPaladin.packets;
+//
+//import ibxm.Player;
+//
+//import java.io.ByteArrayInputStream;
+//import java.io.DataInputStream;
+//import java.io.IOException;
+//
+//import net.minecraft.entity.player.EntityPlayer;
+//import net.minecraft.item.ItemStack;
+//import rpgInventory.gui.rpginv.PlayerRpgInventory;
+//
+///**
+// * 
+// * @author Home
+// */
+//public class RpgPlusPacketHandler implements IPacketHandler {
+//
+//	public static class WEAPONIDS {
+//
+//		public static final int SKULLRCLICK = 5;
+//		public static final int NECROSPECIAL = 6;
+//		public static final int PALADINSPECIAL = 9;
+//	}
+//
+//	@Override
+//	public void onPacketData(INetworkManager manager,
+//			Packet250CustomPayload packet, Player player) {
+//		//
+//		DataInputStream dis = new DataInputStream(new ByteArrayInputStream(
+//				packet.data));
+//		int weaponID = 0;
+//		EntityPlayer p = (EntityPlayer) player;
+//		try {
+//			weaponID = dis.readInt();
+//		} catch (IOException ex) {
+//			ex.printStackTrace();
+//		}
+//		ItemStack weapon = p.getCurrentEquippedItem();
+//		PlayerRpgInventory inv = PlayerRpgInventory.get(p);
+//
+//		if (inv != null) {
+//			switch (weaponID) {
+//
+//			case WEAPONIDS.SKULLRCLICK:
+//				new PacketSpawnMinion(weapon, dis, inv, p);
+//				break;
+//			case WEAPONIDS.NECROSPECIAL:
+//				new PacketNecroSpecial(weapon, dis, inv, p);
+//				break;
+//			case WEAPONIDS.PALADINSPECIAL:
+//				new PacketPalaSpecial(player, p, dis, weapon, inv);
+//				break;
+//			default:
+//				break;
+//			}
+//		}
+//	}
+//}
